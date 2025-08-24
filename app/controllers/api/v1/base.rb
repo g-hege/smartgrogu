f0a5ebaef893f97efdf5646307913d152885e6b6
g-hege <hegedues@gmail.com>
2025-08-24 08:55:39 +0200

    test

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -4,7 +4,7 @@ module API
 
       before do
 #        Rails.logger.info "Received Headers: #{request.headers.to_h.inspect}"
-         error!('401 Unauthorized!', 401) unless headers['x-api-key'] == '12345'
+         error!('401 Unauthorized!!', 401) unless headers['x-api-key'] == '12345'
       end
 
       mount API::V1::DailyRuntime
