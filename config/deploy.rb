e0778916e3efef4cf2568e51444949a6dd6d8fc4
g-hege <hegedues@gmail.com>
2025-08-24 09:07:42 +0200

    puma config

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :deploy_to, "/var/www/smartgrogu"
 # Default value for :pty is false
 # set :pty, true
 
-set :linked_files, %w{config/database.yml config/secrets.yml config/master.key}
+set :linked_files, %w{config/database.yml config/secrets.yml config/master.key config/puma.rb}
 
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
