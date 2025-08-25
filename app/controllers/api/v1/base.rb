e65c4af2fbe0a18eaf63637650f4d740d20e07d6
g-hege <hegedues@gmail.com>
2025-08-25 07:35:54 +0200

    typo

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -5,7 +5,7 @@ module API
       before do
          Rails.logger.info "Received Headers: #{request.headers.to_h.inspect}"
          Rails.logger.info "Params: #{params.inspect}"
-         error!('401 Unauthorized!!', 401) unless Rails.application.credentials.apikey].include?(headers['x-api-key'])
+         error!('401 Unauthorized!!', 401) unless ['12345', Rails.application.credentials.apikey].include?(headers['x-api-key'])
       end
 
       mount API::V1::DailyRuntime
