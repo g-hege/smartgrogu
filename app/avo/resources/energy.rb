9662cc5791c12c353a3137e1506fba474d76b623
g-hege <hegedues@gmail.com>
2025-08-26 08:18:32 +0200

    avo energy

--- app/avo/resources/energy.rb
+++ app/avo/resources/energy.rb
@@ -6,6 +6,13 @@ class Avo::Resources::Energy < Avo::BaseResource
   # }
   
   def fields
-    field :id, as: :id
+    field :id, as: :id, except_on: [:forms, :index]
+    field :day, as: :date, sortable: true
+    field :grid_consumed, as: :number
+    field :real_wiener_netze, as: :number
+    field :solar_self_consumed, as: :number
+    field :solar_to_grid, as: :number
+    field :autarky_rate, as: :number
+    field :self_consumed_rate, as: :number
   end
 end
