ae0c7688a9f8b8f98f73a088c812c2334b86fa49
g-hege <hegedues@gmail.com>
2025-08-26 09:43:19 +0200

    gond job clean error

--- app/jobs/mqtt_start_listener_job.rb
+++ app/jobs/mqtt_start_listener_job.rb
@@ -9,6 +9,8 @@ class MqttStartListenerJob < ApplicationJob
     GoodJob::Job.where(queue_name: 'mqtt_listener').where.not(finished_at: nil).delete_all
     GoodJob::Execution.where(queue_name: 'mqtt_listener').where.not(finished_at: nil).delete_all
 
+    GoodJob::Job.where(queue_name: 'mqtt_listener').update_all( error_event: nil,executions_count: 1, error: nil )
+
     if GoodJob::Job.where(queue_name: 'mqtt_listener', finished_at: nil).count == 0
       MqttListenerJob.perform_later
     end
