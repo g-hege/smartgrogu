176500b186e48aedcd87270614d0bbfa399a8d80
g-hege <hegedues@gmail.com>
2025-08-30 08:08:38 +0200

    config.host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,7 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   config.log_level = :info
+
+  config.hosts << "grogu.ddns.net"
   
 end
