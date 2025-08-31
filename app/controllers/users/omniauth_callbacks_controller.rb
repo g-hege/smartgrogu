c62f9c10e308717e18d10f07d2179aeb781d02f6
g-hege <hegedues@gmail.com>
2025-08-31 10:24:22 +0200

    google_outh

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -20,8 +20,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
       sign_in_and_redirect @user, event: :authentication
     else
-      session["devise.#{session_key}"] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      redirect_to root_path, alert: "Sie haben keine Berechtigung, sich mit diesem Google-Konto anzumelden."
+#      session["devise.#{session_key}"] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+#      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
 end
