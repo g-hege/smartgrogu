53d111087c4fe230570c197e5e0cf2cedb739660
g-hege <hegedues@gmail.com>
2025-09-15 16:35:29 +0200

    totalabrechnung

--- app/services/energy_statistics.rb
+++ app/services/energy_statistics.rb
@@ -1,10 +1,5 @@
 class EnergyStatistics
 
-
-
-
-
-
   # Jahreswerte
   def self.by_year
 
@@ -124,5 +119,122 @@ class EnergyStatistics
       }
     ]
   end
+
+  def self.price_calc(grid_kwh: 0 , netto: 0, month: 1, year: 2025, solar_self_consumed: 0)
+
+#      month = 8
+#      year = 2025
+#      grid_kwh = 307.032
+#      netto = 23.66
+
+      ust = 1.2
+      days_in_year = 365
+      days_in_month = Time.days_in_month(month, year) 
+      days_in_year = Date.new(year).leap? ? 366 : 365
+
+      stromverbrauch = 0
+      netto_preis_kwh = netto / grid_kwh
+      stromverbrauch += netto * ust
+
+      service_fee = (0.0149 * grid_kwh) * ust
+      stromverbrauch += service_fee
+
+      stromherkunftsnachweise = (0.003 * grid_kwh) * ust
+      stromverbrauch += stromherkunftsnachweise
+      
+      grundgebuehr  = (0.0645 * days_in_month) * ust
+      stromverbrauch += grundgebuehr
+
+      gebrauchsabgabe_energie = (netto * 0.06) * ust
+      stromverbrauch += gebrauchsabgabe_energie
+#--
+      netzentgelte = 0
+      netznutzung_ne_7_pauschale_leistung_einfachtarif = (0.074 * grid_kwh) * ust
+      netzentgelte += netznutzung_ne_7_pauschale_leistung_einfachtarif
+
+      netzleistung_ne_7_pauschale_leistung = ((48.0/days_in_year) * days_in_month) * ust
+      netzentgelte += netzleistung_ne_7_pauschale_leistung
+
+      netzverlustentgelt_ne_7 = (0.007 * grid_kwh) * ust
+      netzentgelte += netzverlustentgelt_ne_7
+
+      eag_pauschale_ne_7 = ((19.02/days_in_year)*days_in_month) * ust
+      netzentgelte += eag_pauschale_ne_7
+
+      eag_foerderbeitrag_nicht_gemessene_leistung_ne_7 = ((4.695/days_in_year) * days_in_month) * ust 
+      netzentgelte += eag_foerderbeitrag_nicht_gemessene_leistung_ne_7
+
+      eag_foerderbeitrag_netznutzung_ne_7_nicht_gemessene = (0.0074 * grid_kwh) * ust
+      netzentgelte += eag_foerderbeitrag_netznutzung_ne_7_nicht_gemessene
+
+      eag_foerderbeitrag_netzverlust_ne_7 = (0.0006 * grid_kwh) * ust
+      netzentgelte += eag_foerderbeitrag_netzverlust_ne_7
+
+      messpreis_ebenenbezogen_ne_7 = ((26.16/days_in_year ) * days_in_month) * ust 
+      netzentgelte += messpreis_ebenenbezogen_ne_7
+# find out
+      gebrauchsabgabe_nur_netz_ebenenbezogen_ne_7 = 2.24
+      netzentgelte += gebrauchsabgabe_nur_netz_ebenenbezogen_ne_7
+
+      elektrizitätsabgabe_ohne_ebene = (0.015 * grid_kwh) * ust
+      gebuehren = elektrizitätsabgabe_ohne_ebene
+
+      brutto = stromverbrauch + netzentgelte + gebuehren
+
+      return {month: "#{year}-#{month}",
+              grid_kwh: grid_kwh,
+              stromkosten_netto: stromverbrauch,
+              netzentgelte: netzentgelte,
+              gebuehren: gebuehren,
+              brutto: brutto,
+              brutto_price_kwh: brutto / grid_kwh,
+              solar_self: solar_self_consumed,
+              solar_self_price: solar_self_consumed * (brutto / grid_kwh)
+            }
+
+  end
+
+  def self.invoice_data
+      total = []
+      (2022..Date.current.year).each do |year|
+          ((year==2022 ? 06 : 01) .. Date.current.month).each do |month|
+
+          grid_kwh = Energy.where(Arel.sql("EXTRACT(YEAR FROM day) = #{year}"))
+                 .where(Arel.sql("EXTRACT(MONTH FROM day) = #{month}"))
+                 .sum(:real_wiener_netze)
+
+          netto = Energy.where(Arel.sql("EXTRACT(YEAR FROM day) = #{year}"))
+                 .where(Arel.sql("EXTRACT(MONTH FROM day) = #{month}"))
+                 .sum(:grid_price_netto)
+          solar_self_consumed = Energy.where(Arel.sql("EXTRACT(YEAR FROM day) = #{year}"))
+                 .where(Arel.sql("EXTRACT(MONTH FROM day) = #{month}"))
+                 .sum(:solar_self_consumed)
+
+            total << price_calc(grid_kwh: grid_kwh , netto: netto, month: month, year: year, solar_self_consumed: solar_self_consumed)
+          end
+      end
+      total
+  end
+
+  def self.total_data(invoicedata)
+      totals = invoice_data.each_with_object({
+        grid_kwh: 0,
+        stromkosten_netto: 0,
+        netzentgelte: 0,
+        gebuehren: 0,
+        brutto: 0,
+        solar_self: 0,
+        solar_self_price: 0
+      }) do |item, hash|
+        hash[:grid_kwh] += item[:grid_kwh]
+        hash[:stromkosten_netto] += item[:stromkosten_netto]
+        hash[:netzentgelte] += item[:netzentgelte]
+        hash[:gebuehren] += item[:gebuehren]
+        hash[:brutto] += item[:brutto]
+        hash[:solar_self] += item[:solar_self]
+        hash[:solar_self_price] += item[:solar_self_price]
+      end
+  end
+
 
 end
