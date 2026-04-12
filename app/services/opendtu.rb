f653a8440d4ab5e78e28b459f3e061ef823636ce
g-hege <hegedues@gmail.com>
2026-04-12 09:04:17 +0200

    opendtu api getlivedata

--- app/services/opendtu.rb
+++ app/services/opendtu.rb
@@ -1,66 +1,130 @@
-# app/services/opendtu.rp
-
 class Opendtu
+  # Bequemer Zugriff von außen
+  def self.setlimit(limit)
+    new.setlimit(limit)
+  end
 
-	def self.setlimit(limit)
-		new.setlimit(limit)
-	end
+  def self.getlimit
+  	new.getlimit
+  end
 
-	def setlimit(limit)
+  def self.getlivedata
+    new.getlivedata
+  end
 
-		uri = URI("http://#{Rails.application.credentials.opendtu.ip}/api/limit/config")
-
-		request = Net::HTTP::Post.new(uri.path)
-		request.basic_auth 'admin', Rails.application.credentials.opendtu.pwd
-
-		request["Content-Type"] = "application/x-www-form-urlencoded"
-
-#limit_type = 0 AbsoluteNonPersistent
-#limit_type = 1 RelativeNonPersistent
-#limit_type = 256 AbsolutePersistent
-#limit_type = 257 RelativePersistent
-
-	    json_data = {
-	      serial: Rails.application.credentials.opendtu.serial,
-	      limit_type: 3,
-	      limit_value: limit,
-	    }.to_json
+	def getlivedata
+	    uri = URI("http://#{config.ip}/api/livedata/status")
 	    
-		request.body = "data=#{ERB::Util.url_encode(json_data)}"
+	    request = Net::HTTP::Get.new(uri)
+	    request.basic_auth 'admin', config.pwd
 
-    	http = Net::HTTP.new(uri.host, uri.port)
-    	http.use_ssl = false
-		response = http.request(request)
+	    response = Net::HTTP.start(uri.host, uri.port) { |http| http.request(request) }
 
-		handle_response(response,  limit)
+	    if response.is_a?(Net::HTTPSuccess)
+	      data = JSON.parse(response.body)
+	      
+	      # Suche im Array 'inverters' nach der passenden Serial
+	      inverter = data["inverters"].find { |inv| inv["serial"] == config.serial.to_s }
+	      
+	      if inverter
+	        {
+	          reachable: inverter["reachable"],
+	          producing: inverter["producing"],
+	          power_total: data.dig("total", "Power", "v"), # Aktuelle Gesamtleistung
+	          yield_day: data.dig("total", "YieldDay", "v"), # Tagesertrag
+	          yield_total: data.dig("total", "YieldTotal", "v"), # Ertrag total im kWh
+	          limit_relative: inverter["limit_relative"],
+	          limit_absolute: inverter["limit_absolute"]
+	        }
+	      else
+	        Rails.logger.error "Opendtu: Inverter mit Serial #{config.serial} nicht in Livedaten gefunden."
+	        nil
+	      end
+	    else
+	      Rails.logger.error "Opendtu Live-Data Fehler: #{response.code}"
+	      nil
+	    end
+	  rescue => e
+	    Rails.logger.error "Opendtu Live-Data Exception: #{e.message}"
+	    nil
 	end
 
-	private
-	  def handle_response(response,  limit)
-	    if response.is_a?(Net::HTTPSuccess)
-	      # Erfolgreicher Request (2xx Statuscode)
-			parsed_response = JSON.parse(response.body)
-				if parsed_response["type"] == "success"
-				  Rails.logger.info "Success! Opendtu: #{parsed_response["message"]}"
-				  return true
-				else
-				  Rails.logger.info "Error set Opendtu #{parsed_response["code"]}: #{parsed_response["message"]}"
-				  return false
-				end
-	      return true
-	    elsif response.is_a?(Net::HTTPClientError)
-	      # Client-Fehler (4xx Statuscode)
-	      Rails.logger.error "Opendtu Client-Fehler: #{response.code} #{response.message} #{response.body}"
-	    elsif response.is_a?(Net::HTTPServerError)
-	      # Server-Fehler (5xx Statuscode)
-	      Rails.logger.error "Opendtu Server-Fehler: #{response.code} #{response.message} #{response.body}"
-	    else
-	      # Andere Fehler oder Weiterleitungen (z.B. 3xx)
-	      Rails.logger.error "Opendtu Unbekannter Fehler: #{response.code} #{response.message} #{response.body}"
-	    end
-	    false
-	  end
 
+  def getlimit
+    uri = URI("http://#{config.ip}/api/limit/status")
+    
+    request = Net::HTTP::Get.new(uri)
+    request.basic_auth 'admin', config.pwd
 
+    response = Net::HTTP.start(uri.host, uri.port) { |http| http.request(request) }
 
+    if response.is_a?(Net::HTTPSuccess)
+      data = JSON.parse(response.body)
+      # Wir greifen auf die Seriennummer aus den Credentials zu
+      dtu_data = data[config.serial.to_s]
+      
+      if dtu_data
+        Rails.logger.info "Opendtu Limit: #{dtu_data['limit_relative']}%"
+        return dtu_data["limit_relative"]
+      else
+        Rails.logger.error "Opendtu: Seriennummer #{config.serial} nicht in Antwort gefunden."
+        nil
+      end
+    else
+      Rails.logger.error "Opendtu Get Fehler: #{response.code}"
+      nil
+    end
+  rescue => e
+    Rails.logger.error "Opendtu Exception: #{e.message}"
+    nil
+  end
+
+  def setlimit(limit)
+    uri = URI("http://#{config.ip}/api/limit/config")
+    
+    # HTTP-Client vorbereiten
+    http = Net::HTTP.new(uri.host, uri.port)
+    
+    request = Net::HTTP::Post.new(uri.path)
+    request.basic_auth 'admin', config.pwd
+    request["Content-Type"] = "application/x-www-form-urlencoded"
+
+    json_data = {
+      serial: config.serial,
+      limit_type: 3, # RelativPersistent
+      limit_value: limit.to_i
+    }.to_json
+    
+    request.body = "data=#{ERB::Util.url_encode(json_data)}"
+
+    response = http.request(request)
+    handle_response(response)
+  end
+
+  private
+
+  # Hilfsmethode, um den Zugriff auf Credentials zu verkürzen
+  def config
+    Rails.application.credentials.opendtu
+  end
+
+  def handle_response(response)
+    case response
+    when Net::HTTPSuccess
+      parsed = JSON.parse(response.body)
+      if parsed["type"] == "success"
+        Rails.logger.info "Opendtu Success: #{parsed["message"]}"
+        true
+      else
+        Rails.logger.error "Opendtu API Error: #{parsed["message"]}"
+        false
+      end
+    else
+      Rails.logger.error "Opendtu HTTP Error: #{response.code} - #{response.message}"
+      false
+    end
+  rescue JSON::ParserError
+    Rails.logger.error "Opendtu: Ungültiges JSON vom Device"
+    false
+  end
 end
